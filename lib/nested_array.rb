4a1ac3387a94f75897c1d3ab2a04761651e5e88e
Michael Gerland <mr.gerland@gmail.com>
2019-10-31 19:40:50 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -41,6 +41,7 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
+  
   assembled_matrix[0][2] = "Tiny Ewell"
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
